23154a2f444dd0444218d232408b478c1b090052
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-06-01 11:44:28 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 import numpy as np
-cap = cv.VideoCapture('3686.mp4')
+cap = cv.VideoCapture('/Users/local/PycharmProjects/train/3686.mp4')
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -8,13 +8,14 @@ while cap.isOpened():
     if not ret:
         break
     grayed = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    dst = cv.Canny(grayed, 50, 300, None, 3)
-    blurred = cv.blur(dst, (20, 20))
+    dst = cv.Canny(grayed, 00, 50, None, 3)
+    blurred = cv.blur(dst, (5, 5))
 
 
     thresh = 10
-    img_bw = cv.threshold(blurred, thresh, 255, cv.THRESH_BINARY)[1]
-    cv.imshow("asd", img_bw)
+    #img_bw = cv.threshold(blurred, thresh, 255, cv.THRESH_BINARY)[1]
+    cv.circle(frame, (700, 1000), 20, (0,255,0), -1)
+    cv.imshow("asd", frame)
     if cv.waitKey(30) == ord(' '):
         break
 
